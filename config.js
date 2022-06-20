cf3a563023604f3be4a60f910d624a376e4d3544
niv b <nivb52@gmail.com>
2022-06-20 23:36:18 +0300

    error logger + Improments

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   services_path: './mocks/some_demo_project_folder',
-  project_root: './',
+  project_root: 'C:/',
   debug: true,
 };
--- create_services/index.js
+++ create_services/index.js
@@ -1,9 +1,13 @@
-// const { promisify } = require('util');
+const { promisify } = require('util');
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
+const errorLogger = console.error;
+
 function main(config) {
+  const errorLoggerInDebug = config.debug ? console.error : () => {};
+
   const folders = fs.readdirSync(path.join(config.services_path));
   debug(folders);
   const services = {
@@ -12,58 +16,61 @@ function main(config) {
     choices: folders,
     multiple: true,
   };
-  const { AutoComplete, Toggle } = require('enquirer');
+  const { AutoComplete, Toggle, Input } = require('enquirer');
   const selectServicesPrompt = new AutoComplete(services);
   const errors = [],
     success = [];
-  selectServicesPrompt
-    .run()
-    .then((services_names_to_build) => {
-      debug(services_names_to_build);
-      const service_path_and_version = [];
-      for (let folder_name of services_names_to_build) {
-        const package_json_file = fs.readFileSync(
-          path.join(config.services_path, folder_name, 'package.json'),
-          'utf8'
-        );
-        if (package_json_file) {
-          try {
-            const { version } = JSON.parse(package_json_file);
-            service_path_and_version.push({
-              folder_name,
-              version,
-              serivce_path: path.join(config.services_path, folder_name),
-            });
-          } catch (err) {
-            errors.push({ folder_name: err });
-          }
+  selectServicesPrompt.run().then((services_names_to_build) => {
+    debug(services_names_to_build);
+    const service_path_and_version = [];
+    for (let folder_name of services_names_to_build) {
+      const package_json_file = fs.readFileSync(
+        path.join(config.services_path, folder_name, 'package.json'),
+        'utf8'
+      );
+      if (package_json_file) {
+        try {
+          const { version } = JSON.parse(package_json_file);
+          service_path_and_version.push({
+            folder_name,
+            version,
+            serivce_path: path.join(config.services_path, folder_name),
+          });
+        } catch (err) {
+          errors.push({ folder_name: err });
         }
       }
+    }
 
-      const confimBuildPrompt = new Toggle({
-        message: 'Do you want to docker build all those services ?',
-        enabled: 'Yep',
-        disabled: 'Nope',
-      });
-      confimBuildPrompt.run().then((answer) => {
+    const confimBuildPrompt = new Toggle({
+      message: 'Do you want to docker build all those services ?',
+      enabled: 'Yes',
+      disabled: 'No',
+    });
+    confimBuildPrompt
+      .run()
+      .then((answer) => {
         if (!answer) {
           print('see you some other time');
           return;
         }
-        service_path_and_version.map((ser) => print(ser));
 
         const isTagByBranchPrompt = new Toggle({
           message:
             'Do you want to tag it with the branch (require config of the root) ?',
-          enabled: 'Yep',
-          disabled: 'Nope',
+          enabled: 'Yes',
+          disabled: 'No',
         });
 
-        const onTagByBranchErrorPrompt = new Toggle({
-          message:
-            'There was an error retriving the branch name, do you want to continue anyway (and enter manually the branch) ?',
-          enabled: 'Yep',
-          disabled: 'Nope',
+        const tagByBranchManuallyPrompt = new Toggle({
+          message: 'Do you want enter manually the branch ?',
+          enabled: 'Yes',
+          disabled: 'No',
+        });
+
+        const manualBranchInputPrompt = new Input({
+          message: 'What is your branch?',
+          initial: '',
         });
 
         let tag_by_branch;
@@ -77,34 +84,51 @@ function main(config) {
                 console.log(`Branch: ${tag_by_branch}`);
               } catch (err) {
                 // node couldn't execute the command
-                errors.push({ __git_operation: stderr });
+                errors.push({ __git_operation: err });
                 // ask user if he wants to return or enter the branch manually
-                onTagByBranchErrorPrompt
+                print('There was an error retriving the branch name');
+                tagByBranchManuallyPrompt
                   .run()
                   .then((continue_enter_branch_manually) => {
                     if (!continue_enter_branch_manually) {
                       return;
                     }
+
+                    manualBranchInputPrompt
+                      .run()
+                      .then((answer) => (tag_by_branch = answer))
+                      .catch(errorLoggerInDebug);
                   });
               }
+            } else {
+              tagByBranchManuallyPrompt
+                .run()
+                .then((continue_enter_branch_manually) => {
+                  if (!continue_enter_branch_manually) {
+                    return;
+                  }
+
+                  manualBranchInputPrompt
+                    .run()
+                    .then((answer) => (tag_by_branch = answer))
+                    .catch(errorLoggerInDebug);
+                });
             }
           })
-          .catch(console.error);
+          .then((a) => {
+            print('those builds passed: ');
+            print(success);
+          })
+          .catch(errorLoggerInDebug);
 
         //TODO: build the docker command
         // let the user option to edit the commant
         // exec the command
 
         // print('Answer:', package_json_file);
-      });
-    })
-    .then((res) => {
-      print('those builds faild: ');
-      print(errors);
-      print('those builds passed: ');
-      print(success);
-    })
-    .catch(console.error);
+      })
+      .catch(errorLogger);
+  });
 }
 
 module.exports = main;
